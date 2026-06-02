198720985038d0a6c49190b329f0a72fed1a718c
Glyn Davies <glynwdavies@gmail.com>
2026-06-02 05:41:50 +0100

    Added category service and CORS support.

--- CodePulse.UI/src/app/core/features/category/add-category/add-category.ts
+++ CodePulse.UI/src/app/core/features/category/add-category/add-category.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, effect, inject } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AddCategoryRequest } from '../models/category.model';
+import { CategoryService } from '../services/category-service';
 
 @Component({
   selector: 'app-add-category',
@@ -7,7 +9,22 @@ import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angula
   templateUrl: './add-category.html',
   styleUrl: './add-category.css',
 })
+
 export class AddCategory {
+
+  constructor() {
+    effect(() => {
+      if (this.categoryService.addCategoryStatus() === 'success') {
+        console.log('Category added successfully.');
+        // Redirect back to the category list page or show a success message
+      }
+      if (this.categoryService.addCategoryStatus() === 'error') {
+        console.error('An error occurred while adding the category.');
+      }
+    });
+  }
+
+  private categoryService = inject(CategoryService);
 
   addCategoryFormGroup = new FormGroup({
     name: new FormControl<string>('', { nonNullable: true, validators: [Validators.required, Validators.maxLength(100)] }),
@@ -16,16 +33,27 @@ export class AddCategory {
 
   onSubmit() {
 
-    console.log(this.addCategoryFormGroup.getRawValue());
+    const addCategoryFormValue = this.addCategoryFormGroup.getRawValue();
+
+    const addCategoryRequestDTO: AddCategoryRequest = {
+      name: addCategoryFormValue.name,
+      urlHandle: addCategoryFormValue.urlHandle
+    };
+
+    this.categoryService.addCategory(addCategoryRequestDTO);
 
   }
 
   get nameFormControl() {
+
     return this.addCategoryFormGroup.controls.name;
+
   }
 
   get urlHandleFormControl() {
+
     return this.addCategoryFormGroup.controls.urlHandle;
+
   }
 
 }

8d0ef8a5094345842253dad73c14fa65fe9bbf57
Glyn Davies <glynwdavies@gmail.com>
2026-06-07 09:09:59 +0100

    Created Edit Category component.

--- CodePulse.UI/src/app/app.routes.ts
+++ CodePulse.UI/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { CategoryList } from './core/features/category/category-list/category-list';
 import { AddCategory } from './core/features/category/add-category/add-category';
+import { EditCategory } from './core/features/category/edit-category/edit-category';
 
 export const routes: Routes = [
   { path: 'admin/categories', component: CategoryList },
-  { path: 'admin/categories/add', component: AddCategory}
+  { path: 'admin/categories/add', component: AddCategory },
+  { path: 'admin/categories/edit/:id', component: EditCategory}
 ];
---
+++ CodePulse.UI/src/app/core/features/category/edit-category/edit-category.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-category',
+  imports: [],
+  templateUrl: './edit-category.html',
+  styleUrl: './edit-category.css',
+})
+export class EditCategory {}

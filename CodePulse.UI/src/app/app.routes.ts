816a74c6edfea20a7fe542f4a367ed65f451ba64
Glyn Davies <glynwdavies@gmail.com>
2026-06-09 08:19:58 +0100

    Created BlogPostList component in UI.

--- CodePulse.UI/src/app/app.routes.ts
+++ CodePulse.UI/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { CategoryList } from './core/features/category/category-list/category-list';
 import { AddCategory } from './core/features/category/add-category/add-category';
 import { EditCategory } from './core/features/category/edit-category/edit-category';
+import { BlogpostList } from './core/features/blogpost/blogpost-list/blogpost-list';
 
 export const routes: Routes = [
   { path: 'admin/categories', component: CategoryList },
   { path: 'admin/categories/add', component: AddCategory },
-  { path: 'admin/categories/edit/:id', component: EditCategory}
+  { path: 'admin/categories/edit/:id', component: EditCategory },
+  { path: 'admin/blogposts', component: BlogpostList },
 ];
